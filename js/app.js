6c7aa7507c2643e22aa964a1e337f68856473286
f483953192 <fstofoli@outlook.com>
2022-04-23 19:37:41 -0300

    Ajustada função com o select

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
+window.onload = function(){
+  initSupa()
+}
+
 function initSupa () {
 
   const { createClient } = supabase;
-  const DB_supabase = createClient
-    ('https://hbnnejxzvuzwlmtekpos.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g');
-  
-  
+  const _supabase = createClient
+  ('https://hbnnejxzvuzwlmtekpos.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g');
   const puxaInfo = async () => {
-  let { data: PROVISIONAMENTO, error } = await DB_supabase
+    let { data: PROVISIONAMENTO, error } = await _supabase
     .from('PROVISIONAMENTO')
     .select('tecnicoRua, tipoDeAtivacao, data')
     .eq('tipoDeAtivacao', 'Instalação')
     .eq('tecnicoRua', 'Willians')
     .gte('data', '2022-01-01')
-    .lte('data', '2022-01-31')
-    console.log('DB_supabse',puxaInfo)  
+    .lte('data', '2022-01-10')
+    
     if (error) {
       console.error('erro: ',error)
       return
     }
     console.log('Sucesso', PROVISIONAMENTO);
   }
-  console.log('resulado dentro da função: ',puxaInfo)
+
+  puxaInfo()
 }
-  //console.log('resulado fora da função: ', initSupa)
 
   const ctx = document.getElementById('myChart');
   const myChart = new Chart(ctx, {
