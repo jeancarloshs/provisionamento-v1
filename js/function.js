a60657d36da024d2cfde9a9ebdc7c0471b5e2a01
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2022-05-28 13:58:57 -0300

    Add files via upload
    corrigido função para limpar textarea

--- js/function.js
+++ js/function.js
@@ -28,7 +28,7 @@ function criaScriptProvisionamento (e) {
 
   const _supabase = createClient("https://hbnnejxzvuzwlmtekpos.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g");
 
-  console.log('Supabase Instance: ', _supabase)
+  //console.log('Supabase Instance: ', _supabase)
 
   const main = async () => {
   let { data, error } = await _supabase
@@ -169,4 +169,5 @@ function enviaPlanilha() {
 
 function apagaForm() {
 	document.getElementById('formInformacoes').reset();
+	document.getElementById('scriptOLT').value = "";
 }
