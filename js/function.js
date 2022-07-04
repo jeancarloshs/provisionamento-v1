8d7d2adf472fe406224f82ba97a81e188a66a511
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2022-07-03 21:32:55 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -84,19 +84,19 @@ function criaScriptProvisionamento (e) {
   // }
 
   // else {
-const scriptProvisionamento = (`configure equipment ont interface ${posicionamentoOLT} sw-ver-pland disabled desc1 "${parsedNome}" desc2 "${parsedEndereco}" sernum ${resultado} sw-dnload-version disabled pland-cfgfile1 PREALCO015 dnload-cfgfile1 PREALCO015
-configure equipment ont interface ${posicionamentoOLT} admin-state up
-configure equipment ont slot ${posicionamentoOLT}/14 planned-card-type veip plndnumdataports 1 plndnumvoiceports 0
-configure equipment ont slot ${posicionamentoOLT}/14 admin-state up
-configure qos interface ${posicionamentoOLT}/14/1 upstream-queue 0 bandwidth-profile name:HSI_1G_UP
-configure qos interface ${posicionamentoOLT}/14/1 queue 0 shaper-profile name:HSI_1G_DOWN
-configure qos interface ${posicionamentoOLT}/14/1 upstream-queue 3 bandwidth-profile name:HSI_1M_UP
-configure interface port uni:${posicionamentoOLT}/14/1 admin-up
-configure bridge port ${posicionamentoOLT}/14/1 max-unicast-mac 4
-configure bridge port ${posicionamentoOLT}/14/1 vlan-id 1005 tag single-tagged
-configure bridge port ${posicionamentoOLT}/14/1 vlan-id 202 tag single-tagged
-exit all \n`
-);
+	const scriptProvisionamento = (`configure equipment ont interface ${posicionamentoOLT} sw-ver-pland disabled desc1 "${parsedNome}" desc2 "${parsedEndereco}" sernum ${resultado} sw-dnload-version disabled pland-cfgfile1 PREALCO015 dnload-cfgfile1 PREALCO015
+	configure equipment ont interface ${posicionamentoOLT} admin-state up
+	configure equipment ont slot ${posicionamentoOLT}/14 planned-card-type veip plndnumdataports 1 plndnumvoiceports 0
+	configure equipment ont slot ${posicionamentoOLT}/14 admin-state up
+	configure qos interface ${posicionamentoOLT}/14/1 upstream-queue 0 bandwidth-profile name:HSI_1G_UP
+	configure qos interface ${posicionamentoOLT}/14/1 queue 0 shaper-profile name:HSI_1G_DOWN
+	configure qos interface ${posicionamentoOLT}/14/1 upstream-queue 3 bandwidth-profile name:HSI_1M_UP
+	configure interface port uni:${posicionamentoOLT}/14/1 admin-up
+	configure bridge port ${posicionamentoOLT}/14/1 max-unicast-mac 4
+	configure bridge port ${posicionamentoOLT}/14/1 vlan-id 1005 tag single-tagged
+	configure bridge port ${posicionamentoOLT}/14/1 vlan-id 202 tag single-tagged
+	exit all \n`
+	);
 
   document.getElementById('scriptOLT').value = scriptProvisionamento;
   e.preventDefault();
