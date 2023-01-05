a4ea267079c2a2b35dc413c7345b8989e0da791b
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2023-01-05 00:49:28 -0300

    criado botao sair e funcao para ir para pagina de login e que limpa os dados do localStorage

--- js/addElement.js
+++ js/addElement.js
@@ -1,4 +1,5 @@
 let verificaAdmin = localStorage.getItem('isAdmin', true);
+let token = localStorage.getItem('TokenAuthentication',);
 
 if(verificaAdmin !== 'false') {
   let URL = '<li><a href="opcoes.html">Opções</a></li>'
@@ -7,3 +8,18 @@ if(verificaAdmin !== 'false') {
   newUl.innerHTML = newUl.innerHTML + URL
   //console.log(newUl)
 }
+
+(()=>{
+  let newUl = document.querySelector('ul')
+  let newLi = document.createElement("li")
+  let sair = '<li><a href="#">Sair</li>'
+  newUl.innerHTML = newUl.innerHTML + sair
+
+  addEventListener('click', () =>{
+    localStorage.removeItem("TokenAuthentication");
+    localStorage.removeItem('isAdmin');
+    window.location.assign('index.html')
+    console.log('clicou',event)
+    return '';
+  })
+})()
