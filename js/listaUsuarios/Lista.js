d5a733aca099910028d6f851c3aed97b30a90236
Jean Carlos <jean@naxostelecom.com.br>
2023-01-11 18:01:09 -0300

    adicionado modal - fazer correção do elemento

--- js/listaUsuarios/Lista.js
+++ js/listaUsuarios/Lista.js
@@ -50,6 +50,20 @@ let verificaUsuario = (async () => {
     btnEditar.forEach((element, index) => {
       element.addEventListener('click', () => {
 
+        const openModalButton = element
+        const closeModalButton = document.querySelector("#close-modal");
+        const modal = document.querySelector("#modal");
+        const fade = document.querySelector("#fade");
+
+        const toggleModal = () => {
+          modal.classList.toggle("hide");
+          fade.classList.toggle("hide");
+        };
+
+        [openModalButton, closeModalButton, fade].forEach((el) => {
+          el.addEventListener("click", () => toggleModal());
+        });
+
         console.log('clicou', index)
         
       })
