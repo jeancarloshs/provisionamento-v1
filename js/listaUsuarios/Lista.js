778c83062553a4b38cf0a30c260115a720902600
Jean Carlos <jean@naxostelecom.com.br>
2023-01-24 18:03:48 -0300

    criada função para atualizar e deletar usuarios

--- js/listaUsuarios/Lista.js
+++ js/listaUsuarios/Lista.js
@@ -70,12 +70,12 @@
       element.addEventListener('click', () => {
         
         if (arrayDataJson !== arrayDataJson[index].nomeFuncionario) {
-        console.log('clicou', arrayDataJson[index].nomeFuncionario)
+        //console.log('clicou', arrayDataJson[index].id)
 
           let conteudoModal = document.querySelector('.modal-body')
 
           conteudoModal.innerHTML = `
-          <form id="formInformacoes" method="POST" class="formFormulario formValidation formUsuarios">
+          <form funcionarioID=${arrayDataJson[index].id} id="formInformacoes" method="POST" class="formFormulario formValidation formUsuarios">
       
             <label for="nomeFuncionario"></label>
             <input type="text" id="nomeFuncionario" name="nomeFuncionario" placeholder="Nome Funcionario" data-rules="required|min=3" value="${arrayDataJson[index].nomeFuncionario}">
@@ -99,11 +99,12 @@
       
             <label for="senhaFuncionario"></label>
             <input type="password" name="senhaFuncionario" id="senhaFuncionario" placeholder="Senha" data-rules="required" value="${arrayDataJson[index].senhaFuncionario}">
-      
-            <button type="submit" id="btnSalvar" name="btnSalvar" class="btn btnSalvar">Salvar</button>
-            <button type="submit" id="btnDeletar" name="btnDeletar" class="btn btnDeletar">Deletar</button>
-            <!--<button type="submit" id="btnResetaForm" name="btnResetaForm" class="btn BtnCopiar btnAcoes">Resetar</button>-->
-          </form>`
+
+          </form>
+
+          <button type="submit" id="btnSalvar" name="btnSalvar" class="btn btnSalvar" onclick="btnSalvar()">Salvar</button>
+          <button type="submit" id="btnDeletar" name="btnDeletar" class="btn btnDeletar" onclick="btnDeletar()">Deletar</button>
+          <!--<button type="submit" id="btnResetaForm" name="btnResetaForm" class="btn BtnCopiar btnAcoes">Resetar</button>-->`
        }
       })      
 
@@ -120,10 +121,100 @@
       modal.classList.toggle("hide")
       fade.classList.toggle("hide")
     })
+
+  }
+
+  catch (error) {
+  //console.log('error: ',error)
+  }
+}) // FINAL FUNÇÃO QUE LISTA OS USUARIOS
+();
+
+function btnSalvar() {
+  //const btnSalvar = document.querySelector("#btnSalvar")
+  const formUsuarioID = document.querySelector('.formUsuarios')
+  const idFuncionario = formUsuarioID.getAttribute('funcionarioID')
+  console.log("Usuario Atualizado:", idFuncionario)
+
+  const nomeFuncionario = document.querySelector('#nomeFuncionario').value;
+  const cargoFuncionario = document.querySelector('#tipoDeCargo').value;
+  const permissaoDoColaborador = document.querySelector('#permissaoDoColaborador').value;
+  const emailFuncionario = document.querySelector('#emailFuncionario').value;
+  const senhaFuncionario = document.querySelector('#senhaFuncionario').value;
+
+  (async () => {
+    try {
+      let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g",
+        "Content-Type": "application/json"
+       }
+       
+       let bodyContent = JSON.stringify({   
+        "nomeFuncionario": nomeFuncionario,
+        "cargoFuncionario": cargoFuncionario,
+        "emailFuncionario": emailFuncionario,
+        "senhaFuncionario": senhaFuncionario,
+        "permissaoDoColaborador": permissaoDoColaborador,
+        "update_at": ((new Date()).toISOString()).toLocaleString('br-SP', { hour12: false })
+      });
+      
     
-  } catch (error) {
-    //console.log('error: ',error)
+    let response = await fetch(`https://hbnnejxzvuzwlmtekpos.supabase.co/rest/v1/tbUsuarios?id=eq.${idFuncionario}`, { 
+      method: "PATCH",
+      body: bodyContent,
+      headers: headersList
+    });
+    
+    let data = await response.text();
+    console.log('Usuario Atualizado com Sucesso!!!', data);
+    console.log('Response Status:', response.status)
+    alert(`Usuario ${nomeFuncionario} atualizado com sucesso!!!`)
+  
     } 
 
-})
-();
+    catch (error) {
+      console.log('error: ',error)
+      console.log('Response Status:', response.status)
+    }  
+  })
+  ();
+}// FINAL btnSalvar()
+
+function btnDeletar() {
+  const formUsuarioID = document.querySelector('.formUsuarios')
+  const idFuncionario = formUsuarioID.getAttribute('funcionarioID')
+  console.log("Usuario Deletado:", idFuncionario)
+
+  const nomeFuncionario = document.querySelector('#nomeFuncionario').value;
+
+  (async () => {
+    try {
+      let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g"
+       }
+       
+       let response = await fetch(`https://hbnnejxzvuzwlmtekpos.supabase.co/rest/v1/tbUsuarios?id=eq.${idFuncionario}`, { 
+         method: "DELETE",
+         headers: headersList
+       });
+       
+    let data = await response.text();
+    console.log('Usuario Deletado com Sucesso!!!', data);
+    console.log('Response Status:', response.status)
+    alert(`Usuario ${nomeFuncionario} deletado com sucesso!!!`)
+  
+    }
+
+    catch (error) {
+      console.log('error: ',error)
+      console.log('Response Status:', response.status)
+    }  
+  })
+  ();
+} // FINAL btnDeletar()
