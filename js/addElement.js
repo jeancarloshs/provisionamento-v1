f7a7f9b5b5b441fe4b40e0542ad64b2b0b637a42
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2023-12-16 10:58:40 -0300

    Update addElement.js

--- js/addElement.js
+++ js/addElement.js
@@ -34,12 +34,11 @@ function btnSair(event) {
   try {
     let headersList = {
       "Accept": "*/*",
-      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-      "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g",
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhibm5lanh6dnV6d2xtdGVrcG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA1MDY3MzksImV4cCI6MTk2NjA4MjczOX0.cSPYBGyNvEl_nq69kx3aFfjxWJIqQ-Fdm3EVNPzEA_g"
+      "apikey": "---",
+      "Authorization": "Bearer ---"
      }
      
-     let response = await fetch("https://hbnnejxzvuzwlmtekpos.supabase.co/rest/v1/tbUsuarios", { 
+     let response = await fetch("https://---.supabase.co/rest/v1/tbUsuarios", { 
        method: "GET",
        headers: headersList,
      });
